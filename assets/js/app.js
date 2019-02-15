1c6830ea91812cc6934f1d8991538e5fa930870b
rstutalo <richardtutalo@gmail.com>
2019-02-14 18:22:08 -0700

    stylying almost complete. javascript add for frontend functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 // on click that grabs in the input from the text box when submit is pressed
 $("#button-override").on("click", function() {
-    var address = $("#input-override");
+    var address = $("#input-override").val().trim();
     var queryURL = "http://www.mapquestapi.com/geocoding/v1/address?key=Hh2Y6dWsZuA1C3ZM4fUcz1KEoUUAKHB2&location=" + address;
 // ajax call of the URL
     $.ajax({
@@ -22,3 +22,29 @@ function showResults(lat, lng) {
   console.log(lat)
   console.log(lng)
 }
+
+// ********** SIDEBAR FUNCTIONALITY **********
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("side-nav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("side-nav").style.width = "0";
+
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("side-nav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("side-nav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
+}
+
